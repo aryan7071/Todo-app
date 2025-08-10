82ebb5180b4e02dd5fc813d2e7d77b5a47e741f8
aryan7071 <aryan002005@gmail.com>
2025-08-10 22:59:57 +0530

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState , useEffect } from 'react'
 import './App.css'
 import Navbar from './component/Navbar'
 import { v4 as uuidv4 } from 'uuid';
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 
 function App() {
   const [Todo, setTodo] = useState("")
   const [Todos, setTodos] = useState([]);
 
-  const [showfinished, setshowfinished] = useState(true)
+  const [showfinished, setshowfinished] = useState(true)  
 
 
 
@@ -24,6 +26,11 @@ function App() {
   const savetoLS= (params) => {
     localStorage.setItem("Todos",JSON.stringify(Todos))
   }
+
+  const togglefinished = (e) => {
+    setshowfinished(!showfinished)
+  }
+  
 
   const handleedit = (e, id) => {
     let t = Todos.filter(i=>i.id ===id)
@@ -67,25 +74,31 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto bg-violet-100 my-5 p-5 rounded-xl min-h-[80vh]">
-        <div className="addTodo my-5">
-          <h2 className='text-lg font-bold' >Add a Todo</h2>
-          <input onChange={handlechange} value={Todo} type="text" className='w-1/2  ' />
-          <button onClick={handleadd} className='bg-[#4546E5] hover:bg-[#3838c5] p-2 py-1 text-white text-sm font-bold rounded-md mx-6'>Save</button>
+      <div className=" mx-3 md:container md:mx-auto bg-violet-100 my-5 p-5 rounded-xl min-h-[80vh] md:w-[35%]">
+      <h1 className='font-bold text-3xl text-center' >Todo - Manage your task at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className='text-2xl font-bold' >Add a Todo</h2>
+          <div className="flex">
+
+          <input onChange={handlechange} value={Todo} type="text" className='w-full rounded-full px-5 py-1' />
+          <button onClick={handleadd} disabled={Todo.length<=3} className='bg-[#4546E5] mx-2 hover:bg-[#3838c5] p-4 py-2 text-white text-sm font-bold rounded-full '>Save</button>
+          </div>
         </div>
 
-        <h2 className='text-lg font-bold' >Your Todos</h2>
+        <input className='my-4 mx-3' onChange={togglefinished} type="checkbox" checked={showfinished} />Show Finished
+        <div className='h-[1px] opacity-25 bg-black mx-auto my-2 w-[90%]'></div>
+        <h2 className='text-2xl font-bold' >Your Todos</h2>
         <div className="todos">
           {Todos.length == 0 && <div className='m-5' >No Todos to display </div>}
           {Todos.map(item => {
-            return <div key={item.id} className="Todo flex my-3 justify-between w-1/2">
-              <div className='flex gap-5' >
-                <input name={item.id} onChange={handlecheckbox} type="checkbox" value={item.iscompleted} id='' />
+            return (showfinished || !item.iscompleted) && <div key={item.id} className="Todo flex my-3 justify-between ">
+              <div className='flex  gap-5' >
+                <input name={item.id} onChange={handlecheckbox} type="checkbox" checked={item.iscompleted} id='' />
                 <div className={item.iscompleted ? "line-through" : ""}>{item.Todo}</div>
               </div>
               <div className="buttons flex h-full">
-                <button onClick={(e)=>handleedit(e,item.id)} className='bg-[#4546E5] hover:bg-[#3838c5] p-2 py-1 text-white text-sm font-bold rounded-md mx-1'>Edit</button>
-                <button onClick={(e) => { handledelete(e, item.id) }} className='bg-[#4546E5] hover:bg-[#3838c5] p-2 py-1 text-white text-sm font-bold rounded-md mx-1'>Delete</button>
+                <button onClick={(e)=>handleedit(e,item.id)} className='bg-[#4546E5] hover:bg-[#3838c5] p-2 py-1 text-white text-sm font-bold rounded-md mx-1'><FaEdit /></button>
+                <button onClick={(e) => { handledelete(e, item.id) }} className='bg-[#4546E5] hover:bg-[#3838c5] p-2 py-1 text-white text-sm font-bold rounded-md mx-1'><AiFillDelete />  </button>
               </div>
             </div>
           })}
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between  bg-white shadow-md text-black py-2 ' > 
+    <nav className='flex justify-around  bg-white shadow-md text-black py-2 ' > 
     <div className="logo">
       <span className='font-bold text-xl mx-9 text-indigo-600 cursor-pointer ' > 📝 TodoList </span>
     </div>
